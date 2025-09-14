ce3a4bedb0eb812a5459e95993615614a242ac6e
Jua Sebastian Arango Quiroga <juan.sebastian.arango@correounivalle.edu.co>
2025-09-14 13:49:04 -0500

    se crea carpetas global y locas para archivos HTML, CSS, JS y img

--- licoreria/settings.py
+++ licoreria/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'licoreria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# para carpetas globales
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
