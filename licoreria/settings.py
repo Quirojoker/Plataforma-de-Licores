5842990108a3c0515e7eb97316a3cbac6be8c3a3
Jua Sebastian Arango Quiroga <juan.sebastian.arango@correounivalle.edu.co>
2025-12-20 13:42:18 -0500

    se crear archivo .env y ajustes en settings para seguridad basica

--- licoreria/settings.py
+++ licoreria/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w#5b!wp0wnda!6ob3%jk_ed$ka-_9ht^ogcs&_k(-#6ifu0o!%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'licoreria.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'licoreria_db',
-        'USER':'postgres',
-        'PASSWORD': 'lasbolasdeldragon',
-        'HOST':'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
